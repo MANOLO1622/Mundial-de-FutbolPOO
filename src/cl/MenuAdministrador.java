fc0d558c6a28dac5b1f5e5262d9a0a2a82ba1cb2
Manuel Elizondo <manolobe23@hotmail.com>
2018-07-22 15:19:33 -0600

    Cambio de botones se elimino crear copas

--- src/cl/MenuAdministrador.java
+++ src/cl/MenuAdministrador.java
@@ -15,7 +15,7 @@ public class MenuAdministrador extends JPanel{
 	JButton boton10;
 	JButton boton11;
 	JButton boton12;
-	JButton boton13;
+
 
 	
 	
@@ -46,15 +46,11 @@ public class MenuAdministrador extends JPanel{
 		boton11.setBounds(10, 325, 180, 50);
 		this.add(boton11);
 		
-		boton12 = new JButton("Crear copas");
-		this.setLayout(null);
-		boton12.setBounds(10, 400, 180, 50);
-		this.add(boton12);
 		
-		boton13 = new JButton("Crear ligas");
+		boton12 = new JButton("Crear ligas");
 		this.setLayout(null);
-		boton13.setBounds(10, 475, 180, 50);
-		this.add(boton13);
+		boton12.setBounds(10, 475, 180, 50);
+		this.add(boton12);
 		
 	 
 		
