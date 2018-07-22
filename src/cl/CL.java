63ba0193532a17cb220bd8e21549f20e0b069b5c
Manuel Elizondo <manolobe23@hotmail.com>
2018-07-22 15:40:29 -0600

    Se agrego el codigo respectivo el CL, GESTOR De mundiales

--- src/cl/CL.java
+++ src/cl/CL.java
@@ -7,11 +7,17 @@ import java.util.*;
 public class CL {
 	
 	private static ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
+	private static ArrayList<Mundiales> listaMundiales = new ArrayList<Mundiales>();
 	private static TextFileIO UsuariosRegLog = new TextFileIO("logs//Login.txt");
 	
 	public static void registrarUsuario(Usuario registro) {
 		
 		listaUsuarios.add(registro);
+		
+	}
+	public static void registrarMundial(Mundiales registro) {
+		
+		listaMundiales.add(registro);
 		
 	}
 	
@@ -20,6 +26,12 @@ public class CL {
 		return listaUsuarios;
 		
 	}
+	public static ArrayList<Mundiales> listarMundiales() {
+		
+		return listaMundiales;
+		
+	}
+	
 	
 	
 }
