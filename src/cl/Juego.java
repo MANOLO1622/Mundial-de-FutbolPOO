0f0590831d75d02902ece1bb7249e27e86441c41
Manuel Elizondo <manolobe23@hotmail.com>
2018-07-25 14:22:42 -0600

    se cambio la imagen de fondo del juego

--- src/cl/Juego.java
+++ src/cl/Juego.java
@@ -10,8 +10,7 @@ import gestor.Gestor;
 
 public class Juego extends JPanel implements ActionListener{
 	
-	private Usuario miUsuarioActual;
-	
+	public Usuario miUsuario;
 	private static Image imagen;
 	private static Gestor controlador = new Gestor();
 	
@@ -22,19 +21,7 @@ public class Juego extends JPanel implements ActionListener{
 	private JLabel bandera = new JLabel();
 	private ImageIcon banderaImagen;
 	
-	private JLabel informacionLiga1 = new JLabel("Informacion de la Liga");
-	private JLabel informacionLiga2 = new JLabel("");
-	private JLabel informacionLiga3 = new JLabel("");
-	private JLabel informacionLiga4 = new JLabel("");
-	private JLabel informacionLiga5 = new JLabel("");
-	private JLabel informacionLiga6 = new JLabel("");
-	
-	
-	
-	
-	public Juego(Usuario miUsuarioActual) {
-		
-		this.miUsuarioActual = miUsuarioActual;
+	public Juego() {
 		
 		CL capaLogica = new CL();
 		ArrayList<Equipo> listaEquipos = capaLogica.listarEquiposFIFA();
@@ -45,7 +32,7 @@ public class Juego extends JPanel implements ActionListener{
 		ligas.setBounds(10, 10, 100, 25);
 		ligas.addItem("Liga Publica");
 		ligas.addItem("Liga Privada");
-	
+		
 		this.add(equipos);
 		equipos.setBounds(120, 10, 100, 25);
 		
@@ -60,8 +47,8 @@ public class Juego extends JPanel implements ActionListener{
 			
 		}
 		
+		
 		equipos.addActionListener(this);
-		ligas.addActionListener(this);
 		
 	}
 		
@@ -69,6 +56,7 @@ public class Juego extends JPanel implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		
 		if(e.getSource() == equipos) {
 			
@@ -80,123 +68,13 @@ public class Juego extends JPanel implements ActionListener{
 			
 		}
 		
-		if(e.getSource()==ligas) {
-			
-			if(ligas.getSelectedIndex() == 0 ) {
-				
-				if(this.miUsuarioActual.getMiLigaPublica() == null) {
-					
-					mostrarInformacionLiga(2);
-					JOptionPane.showMessageDialog(null, "No hay liga Publica asociada al Jugador " + this.miUsuarioActual.getAvatar());
-					
-					
-				}else {
-					
-					mostrarInformacionLiga(0);
-					
-				}
-				
-			}
-			else if(ligas.getSelectedIndex() == 1) {
-				
-				if(this.miUsuarioActual.getMiLigaPrivada() == null) {
-					
-					mostrarInformacionLiga(2);
-					JOptionPane.showMessageDialog(null, "No hay liga Privada asociada al Jugador " + this.miUsuarioActual.getAvatar());
-					
-				}else {
-					
-					mostrarInformacionLiga(1);
-					
-				}
-				
-			}
-			
-		}
-		
 	}
 	
 	
 	
 	
 	
-	public void mostrarInformacionLiga(int tipoLiga) {
-		
-		this.add(informacionLiga1);
-		informacionLiga1.setBounds(10,110, 600,100);
-		informacionLiga1.setForeground(Color.WHITE);
-		informacionLiga1.setFont(new Font(informacionLiga1.getFont().getFontName(), Font.PLAIN, 30));
-		
-		this.add(informacionLiga2);
-		informacionLiga2.setBounds(10,160, 600,100);
-		informacionLiga2.setForeground(Color.WHITE);
-		informacionLiga2.setFont(new Font(informacionLiga2.getFont().getFontName(), Font.PLAIN, 30));
-		
-		this.add(informacionLiga3);
-		informacionLiga3.setBounds(10,190, 600,100);
-		informacionLiga3.setForeground(Color.WHITE);
-		informacionLiga3.setFont(new Font(informacionLiga3.getFont().getFontName(), Font.PLAIN, 30));
-		
-		this.add(informacionLiga4);
-		informacionLiga4.setBounds(10,220, 600,100);
-		informacionLiga4.setForeground(Color.WHITE);
-		informacionLiga4.setFont(new Font(informacionLiga4.getFont().getFontName(), Font.PLAIN, 30));
-		
-		this.add(informacionLiga5);
-		informacionLiga5.setBounds(10,250, 600,100);
-		informacionLiga5.setForeground(Color.WHITE);
-		informacionLiga5.setFont(new Font(informacionLiga5.getFont().getFontName(), Font.PLAIN, 30));
-		
-		this.add(informacionLiga6);
-		informacionLiga6.setBounds(10,280, 600,100);
-		informacionLiga6.setForeground(Color.WHITE);
-		informacionLiga6.setFont(new Font(informacionLiga6.getFont().getFontName(), Font.PLAIN, 30));
-		
-
-		switch(tipoLiga) {
-		
-		case 0:
-			informacionLiga1.setText("Informacion de la Liga " + this.miUsuarioActual.getMiLigaPublica().getNombreLiga());
-			informacionLiga2.setText("Nombre de la Liga: " + this.miUsuarioActual.getMiLigaPublica().getNombreLiga());
-			informacionLiga3.setText("Fecha de creacion: " + this.miUsuarioActual.getMiLigaPublica().getFechaCreacion());
-			informacionLiga4.setText("Estado: " + this.miUsuarioActual.getMiLigaPublica().getEstado());
-			informacionLiga5.setText("Puntos: " + this.miUsuarioActual.getMiLigaPublica().getPuntos());
-			informacionLiga6.setText("Bono: " + this.miUsuarioActual.getMiLigaPublica().getBono());	
-			
-			break;
-			
-		case 1:
-			informacionLiga1.setText("Informacion de la Liga " + this.miUsuarioActual.getMiLigaPrivada().getNombreLiga());
-			informacionLiga2.setText("Nombre de la Liga: " + this.miUsuarioActual.getMiLigaPrivada().getNombreLiga());
-			informacionLiga3.setText("Fecha de creacion: " + this.miUsuarioActual.getMiLigaPrivada().getFechaCreacion());
-			informacionLiga4.setText("Estado: " + this.miUsuarioActual.getMiLigaPrivada().getEstado());
-			informacionLiga5.setText("Puntos: " + this.miUsuarioActual.getMiLigaPrivada().getPuntos());
-			informacionLiga6.setText("Bono: " + this.miUsuarioActual.getMiLigaPrivada().getBono());
-			
-			break;
-			
-		case 2: 
-			
-			informacionLiga1.setText("");
-			informacionLiga2.setText("");
-			informacionLiga3.setText("");
-			informacionLiga4.setText("");
-			informacionLiga5.setText("");
-			informacionLiga6.setText("");
-			
-			break;
-			
-		default:
-			
-			JOptionPane.showMessageDialog(null, "Valor invalido\n1 para liga privada.\n0 para liga publica.");
-			
-			break;
-		
-		
-		}
-		
-		
-	}
+	
 	
 	
 	
