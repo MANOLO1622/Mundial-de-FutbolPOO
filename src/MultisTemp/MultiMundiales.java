480e80d0473de23d1bdd5a84db2c2215464bc35b
Manuel Elizondo <manolobe23@hotmail.com>
2018-08-12 20:29:51 -0600

    Se agregaron las multis requeridas, NOTA hay algunos BUG que comente ya que aun falta por corregir

---
+++ src/MultisTemp/MultiMundiales.java
@@ -0,0 +1,81 @@
+package MultisTemp;
+
+import java.sql.ResultSet;
+
+import cl.*;
+
+public class MultiMundiales {
+
+	public Mundiales crear(String pnombreMundial, String pano, String ppaisOrganizador, Boolean pestado) {
+		Mundiales Mundiales = null;
+		String sql;
+		sql = "INSERT INTO Mundiales " + "(nombreMundial, ano , paisOrganizador, estado) " + "VALUES ('"
+				+ pnombreMundial + "', '" + pano + "','" + ppaisOrganizador + "','" + pestado + "');";
+		try {
+
+			//Conector.getConector().ejecutarSQL(sql);
+
+			//Mundiales = new Mundiales(pnombreMundial, pano, ppaisOrganizador, pestado); Esta comentado porque tiene  un Bug pero hay que usarlo
+		} catch (Exception e) {
+			System.out.println("Exception " + e.toString());
+
+		}
+		return Mundiales;
+	}
+
+	public Mundiales buscar(String pnombreMundial) throws java.sql.SQLException, Exception {
+		Mundiales Mundiales = null;
+		java.sql.ResultSet rs = null;
+		String sql;
+		sql = "SELECT nombreMundial" + "FROM Mundiales " + "WHERE nombreMundial ='" + pnombreMundial + ";";
+
+
+
+		/*if (rs.next()) {
+			Mundiales = new Mundiales(rs.getString("nombreMundial"), rs.getString("Ano"), rs.getString("paisOrganizador"),
+					rs.getBoolean("estado");
+		} else {
+			throw new Exception("El Mundial no está registrado.");Esta comentado porque tiene  un Bug pero hay que usarlo
+		}
+		rs.close();*/
+		return Mundiales;
+	}
+
+	public void actualizar(Mundiales pMundiales) throws java.sql.SQLException, Exception {
+		String sql;
+		sql = "UPDATE Mundiales " + "SET nombreMundial='" + pMundiales.getNombreMundial() + "';";
+		try {
+			
+
+		} catch (Exception e) {
+			throw new Exception("El Mundial no está registrado.");
+		}
+	}
+
+	public void borrar(Mundiales pMundiales) throws java.sql.SQLException, Exception {
+		String sql;
+		sql = "DELETE FROM Mundiales " + "WHERE nombre='" + pMundiales.getNombreMundial()+ "';";
+		try {
+		
+
+		} catch (Exception e) {
+			throw new Exception("El Mundial tiene cuentas.");
+		}
+	}
+
+	/*public String listarMundiales() throws java.sql.SQLException, Exception {
+		String sql, lista = "";
+		sql = "SELECT * FROM Mundiales";
+		try {
+			ResultSet rs = null;
+			rs = Conector.getConector().ejecutarSQL(sql, true);
+			while (rs.next()) {
+				lista += "NombreMundial: " + rs.getString("nombreMundial") + ", Ano: " + rs.getString("ano") + ", PaisOrganizador: "
+						+ rs.getString("paisOrganizador") + ", Estado: " + rs.getBoolean("estado");
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR NO ESTA LISTANDO " + e.toString());  Esta comentado pero hay que usarlo
+		}
+		return lista;
+	}*/
+}
---
+++ src/MultisTemp/MultiUsuarios.java
@@ -0,0 +1,83 @@
+package MultisTemp;
+
+import cl.*;
+
+public class MultiUsuarios {
+
+	public Usuario crear(String pnombre, String papellido, String pnombreUsuario, String pavatar, String pcorreoElectronico,
+			String pcontrasena) {
+		Usuario Usuario = null;
+		String sql;
+		sql = "INSERT INTO Usuario " + "(nombre, apellido,nombreUsuario, avatar, correoElectronico, contrasena ) " + "VALUES ('"
+				+ pnombre + "', '" + papellido + "','" + pnombreUsuario + "','" + pavatar + "','" + pcorreoElectronico + "','"
+				+ pcontrasena + "');";
+		try {
+
+			//Conector.getConector().ejecutarSQL(sql);
+
+			//Usuario = new Usuario(pnombre, papellido, pnombreUsuario, pavatar, pcorreoElectronico, pcontrasena); Esta comentado porque tiene  un Bug pero hay que usarlo
+		} catch (Exception e) {
+			System.out.println("Exception " + e.toString());
+
+		}
+		return Usuario;
+	}
+
+	public Usuario buscar(String pnombre) throws java.sql.SQLException, Exception {
+		Usuario Usuario = null;
+		java.sql.ResultSet rs = null;
+		String sql;
+		sql = "SELECT nombre" + "FROM Usuario " + "WHERE nombre='" + pnombre + "';";
+
+
+
+		/*if (rs.next()) {
+			Usuario = new Usuario(rs.getString("nombre"), rs.getString("apellido"), rs.getString("nombreUsuario"),
+					rs.getString("avatar"), rs.getString("correoElectronico"), rs.getString("contrasena"));Esta comentado porque tiene  un Bug pero hay que usarlo
+		} else {
+			throw new Exception("El Usuario no está registrado.");
+		}*/
+		rs.close();
+		return Usuario;
+	}
+
+	public void actualizar(Usuario pUsuario) throws java.sql.SQLException, Exception {
+		String sql;
+		sql = "UPDATE Usuario " + "SET nombre='" + pUsuario.getNombre() + "' " + "WHERE apellido='"
+				+ pUsuario.getApellido() + "';";
+		try {
+			
+
+		} catch (Exception e) {
+			throw new Exception("El Usuario no está registrado.");
+		}
+	}
+
+	public void borrar(Usuario pUsuario) throws java.sql.SQLException, Exception {
+		String sql;
+		sql = "DELETE FROM Usuario " + "WHERE nombre='" + pUsuario.getNombre() + "';";
+		try {
+		
+
+		} catch (Exception e) {
+			throw new Exception("El Usuario tiene cuentas.");
+		}
+	}
+
+	/*public String listarUsuario() throws java.sql.SQLException, Exception {
+		String sql, lista = "";
+		sql = "SELECT * FROM Usuario";
+		try {
+			ResultSet rs = null;
+			rs = Conector.getConector().ejecutarSQL(sql, true);
+			while (rs.next()) {
+				lista += "Nombre: " + rs.getString("nombre") + ", Apellido: " + rs.getString("apellido") + ", NombreUsuario: "
+						+ rs.getString("nombreUsuario") + ", Avatar: " + rs.getString("avatar") + ", CorreoElectronico: "
+						+ rs.getString("correoElectronico") + ", Contrasena: " + rs.getString("contrasena");
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR NO ESTA LISTANDO " + e.toString());
+		}
+		return lista;
+	}*/
+}
