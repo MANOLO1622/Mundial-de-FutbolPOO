973cb9c825bd98622a3d420e18ce2f30f0b1f9e9
Dennis Ly <Dennis Ly@DESKTOP-ERCOI9T.mshome.net>
2018-08-17 16:56:09 -0500

    Se agrego el paquete de acceso de datos para la lectura de el archivo de texto que contiene el String de conexion.

---
+++ src/accesoDatos/Conector.java
@@ -0,0 +1,25 @@
+package accesoDatos;
+
+import java.util.ArrayList;
+
+ 
+public class Conector{
+		
+	private static AccesoBD conectorBD = null;
+	
+	//--------------------------------------------------------------------------------
+	
+	public static AccesoBD getConector() throws java.sql.SQLException,Exception{
+		if(conectorBD==null){
+			
+			TextFileIO lector = new TextFileIO("Datos Conexion.txt");
+			ArrayList<String> valoresConexion = lector.getData();
+			
+			conectorBD = new AccesoBD(valoresConexion.get(0),valoresConexion.get(1));
+		
+		}
+		
+		return conectorBD;
+	}
+	
+}
