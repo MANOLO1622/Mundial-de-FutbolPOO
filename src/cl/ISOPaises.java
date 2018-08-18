628fa4e9eaea3461543e5db0c821e7a9888ac286
Dennis Ly <Dennis Ly@DESKTOP-ERCOI9T.mshome.net>
2018-08-18 12:11:59 -0500

    -

--- src/cl/ISOPaises.java
+++ src/cl/ISOPaises.java
@@ -2,11 +2,68 @@ package cl;
 
 public enum ISOPaises {
 	
-	DE,BR,BE,PT,AR,CH,FR,PL,CL,ES,PE,DK
-	
+	DE,BR,BE,PT,AR,CH,FR,PL,CL,ES,PE,DK,CUSTOM;
 	
 	//https://www.fifa.com/fifa-world-ranking/ranking-table/men/index.html
 	//https://www.worldatlas.com/aatlas/ctycodes.htm
 	
 
+	public static ISOPaises retornarISOPais(String iso) {
+		
+		if(iso.equals("DE")) {
+			
+			return ISOPaises.DE;
+			
+		}else if(iso.equals("BR")) {
+			
+			return ISOPaises.BR;
+			
+		}else if(iso.equals("BE")) {
+			
+			return ISOPaises.BE;
+			
+		}else if(iso.equals("PT")) {
+			
+			return ISOPaises.PT;
+			
+		}else if(iso.equals("AR")) {
+			
+			return ISOPaises.AR;
+			
+		}else if(iso.equals("CH")) {
+			
+			return ISOPaises.CH;
+			
+		}else if(iso.equals("FR")) {
+			
+			return ISOPaises.FR;
+			
+		}else if(iso.equals("PL")) {
+			
+			return ISOPaises.PL;
+			
+		}else if(iso.equals("CL")) {
+			
+			return ISOPaises.CL;
+			
+		}else if(iso.equals("ES")) {
+			
+			return ISOPaises.ES;
+			
+		}else if(iso.equals("PE")) {
+			
+			return ISOPaises.PE;
+			
+		}else if(iso.equals("DK")) {
+			
+			return ISOPaises.DK;
+			
+		}else {
+			
+			return ISOPaises.CUSTOM;
+		}
+		
+		
+	}
+	
 }
